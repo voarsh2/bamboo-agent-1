5ecd772d2b0b6187e68011abf773c78d87fd5161
Reese Jenner <reesevader@hotmail.co.uk>
2023-08-02 15:15:46 +0100

    Fix docker start race condition
    The code now waits for the Docker service to be running before continuing. This prevents a race condition where subsequent commands may fail due to Docker not being fully started yet.

--- scripts/start/docker.sh
+++ scripts/start/docker.sh
@@ -6,8 +6,13 @@ fi
 
 usermod -aG docker bamboo
 
-service docker start
-service docker status
+while true; do
+    service docker start
+    sleep 3
+    if service docker status | grep -q "running"; then
+        break
+    fi
+done
 
 chmod a+wrx /var/run/docker.sock
 ls -l /var/run/docker.sock
