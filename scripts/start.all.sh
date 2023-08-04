0261a87a7e2ccadc74451aea2dfd7cadc985146f
Reese Jenner <reesevader@hotmail.co.uk>
2023-08-04 03:16:49 +0100

    feat: Improve Supervisor and Docker start scripts
    - Added retry mechanism for starting Supervisor and Docker
    - Displayed success message when Supervisor and Docker started successfully
    - Displayed error messages when there are failures in starting Supervisor and Docker
    - Exited with an error if the retry limit is exceeded

--- scripts/start.all.sh
+++ scripts/start.all.sh
@@ -9,10 +9,29 @@ export DEBUG_MODE=true
   exit 1
 }
 
-# Start Supervisor
-{
- sudo -s supervisord -c /etc/supervisor/conf.d/supervisord.conf
-} || {
-  echo "Something went wrong - supervisor start (start.all)"
+# Start Supervisor with retry
+retry_count=0
+retry_limit=10
+while true; do
+  if sudo -s supervisord -c /etc/supervisor/conf.d/supervisord.conf; then
+    echo "Supervisor started successfully"
+    break
+  else
+    echo "Something went wrong - Supervisor start (start.all)"
+    retry_count=$((retry_count+1))
+    if [ $retry_count -eq $retry_limit ]; then
+      echo "Exceeded retry limit, exiting..."
+      exit 1
+    fi
+    echo "Retrying in 1 second..."
+    sleep 1
+  fi
+done
+
+# Check if Supervisor is running
+if sudo -s supervisorctl status; then
+  echo "Supervisor is running"
+else
+  echo "Supervisor is not running"
   exit 1
-}
+fi
--- scripts/start/docker.sh
+++ scripts/start/docker.sh
@@ -6,12 +6,22 @@ fi
 
 usermod -aG docker bamboo
 
+retry_count=0
+retry_limit=10
+
 while true; do
-    service docker start
-    sleep 3
-    if service docker status | grep -q "running"; then
-        break
+  if service docker start; then
+    echo "Docker started successfully"
+    break
+  else
+    echo "Failed to start Docker, retrying..."
+    retry_count=$((retry_count+1))
+    if [ $retry_count -eq $retry_limit ]; then
+      echo "Exceeded retry limit, exiting..."
+      exit 1
     fi
+    sleep 3
+  fi
 done
 
 chmod a+wrx /var/run/docker.sock
