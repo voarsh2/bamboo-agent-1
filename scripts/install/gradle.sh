df23d39de4ca7faf65ac9a713e4b9a0990ff6341
Reese Jenner <reesevader@hotmail.co.uk>
2023-10-24 02:03:50 +0100

    Tidy up

--- scripts/install/gradle.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Download and unzip Gradle distribution
-wget https://services.gradle.org/distributions/gradle-7.4-all.zip -P /tmp && \nzip /tmp/gradle-7.4-all.zip -d /opt/gradle && \
-    rm /tmp/gradle-7.4-all.zip
-
-# Set environment variables
-echo "export GRADLE_HOME=/opt/gradle/gradle-7.4" >> /etc/environment
-echo "export PATH=\$PATH:\$GRADLE_HOME/bin" >> /etc/environment
